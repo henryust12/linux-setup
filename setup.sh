9f1c06c152a09389b2e085787f8e4bcac704be27
Simarmata <henryust12@GMAIL.COM>
2024-04-12 03:07:12 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/dbeaver_ce/dbeaver_ce.sh | bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/docker/docker.sh | bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/group_timeout/group_timeout.sh | bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/keyboard_layout/japanese.sh | bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/node/node.sh | bash
+
+curl -sL https://raw.githubusercontent.com/henryust12/linux-setup/lmde-faye-bookworm/shopify/shopify.sh | bash
